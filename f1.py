bf64b002d53495e239f044ba4a1d8c28a244482b
ljb <hikboy@outlook.com>
2021-04-17 23:05:45 +0800

    add new function examples

---
+++ f1.py
@@ -0,0 +1,11 @@
+def greet_user():
+	"""Display a simple greeting."""
+	print("hello!")
+
+greet_user()
+
+def gree_name(username):
+	"""Display a real user"""
+	print(f"hello {username}")
+
+gree_name("zhansan")
